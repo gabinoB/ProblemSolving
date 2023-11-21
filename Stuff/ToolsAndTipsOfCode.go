e87f8914fe7130e64be55a7c2bb7926826fa25fd
GabinoB <58829003+gabinoB@users.noreply.github.com>
2023-11-21 14:57:20 -0300

    Update ToolsAndTipsOfCode.go
    Correccion de ubicacion de variables en el sistema de conversion de string a float desde un input de consola

--- Stuff/ToolsAndTipsOfCode.go
+++ Stuff/ToolsAndTipsOfCode.go
@@ -30,8 +30,8 @@ line, _ := in.ReadString('\n')   //En "line" es donde se guarda la string ingres
 
 // Si se ingresó un numero como string y se lo desea convertir a float64:
 
-valueYetStringType = strings.TrimSpace(price) //Quita el espacio al final de la string para poder convertirlo correctamente a float.
-valueConvertedToFloat64, _ := strconv.ParseFloat(price, 8)
+valueYetStringType = strings.TrimSpace(line) //Quita el espacio al final de la string para poder convertirlo correctamente a float.
+valueConvertedToFloat64, _ := strconv.ParseFloat(valueYetStringType, 8)
 
 */
 
